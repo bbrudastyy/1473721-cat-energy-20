644d0b2f5060be1af751d69eff242685804f8570
Богдан Котов <bbrudastyy@gmail.com>
2020-08-17 15:43:54 +0300

    Для консультации

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,12 @@ const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const gulpCopy = require("gulp-copy");
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
 
 // Styles
 
@@ -16,8 +22,10 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style-min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
@@ -28,7 +36,7 @@ exports.styles = styles;
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -49,3 +57,67 @@ const watcher = () => {
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+//Image min
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+}
+
+exports.image = images;
+
+//Sprite
+
+const sprite = () => {
+  return gulp.src("source/img/**/sprite-*.svg")
+  .pipe(svgstore())
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"))
+}
+
+exports.sprite = sprite;
+
+//Copy
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+};
+
+exports.copy = copy;
+
+//Del
+
+const clean = () => {
+  return del("build");
+};
+
+exports.clean = clean;
+
+//Build
+
+const build = () => gulp.series(
+  "clean",
+  "copy",
+  "styles",
+  "sprite",
+  "html"
+);
+
+exports.build = build;
+
+gulp.task("build", gulp.series("clean", "copy", "styles", "sprite", "html"));
+gulp.task("start", gulp.series("build", "start"));
+
+
