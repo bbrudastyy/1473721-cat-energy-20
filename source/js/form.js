ced634624d3a7ea45a5d935e9973b4f51198e46c
Богдан Котов <bbrudastyy@gmail.com>
2020-08-24 15:20:28 +0300

    Исправил замечания перед защитой от наставника

--- source/js/form.js
+++ source/js/form.js
@@ -1,17 +1,24 @@
 var form = document.querySelector(".selection__form");
-if (form){
+if (form) {
   var validateBtn = form.querySelector(".selection__btn");
   var requireds = form.querySelectorAll(".form-required");
+  var icons = form.querySelectorAll(".cat-owner__icon");
 
   validateBtn.addEventListener('click', function (event) {
     event.preventDefault();
 
     for (var i = 0; i < requireds.length; i++) {
-      requireds[i].classList.remove("error");
+      requireds[i].classList.remove("error--field");
+      if (i > 2) {
+        icons[i - 2].classList.remove("error--icon");
+      }
       if (!requireds[i].value) {
-        requireds[i].classList.add("error");
+        requireds[i].classList.add("error--field");
+        if (i > 1) {
+          icons[i - 2].classList.add("error--icon");
+        }
         window.scrollTo(0, 0);
       }
     }
   });
-  }
+}
